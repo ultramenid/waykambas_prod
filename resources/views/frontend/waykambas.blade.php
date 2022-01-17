c432d4cd7440cca3762a5abbe3a4ae4010ee8173
ultramenid <malichamdan@gmail.com>
2022-01-17 19:14:31 +0700

    max width pages

--- resources/views/frontend/waykambas.blade.php
+++ resources/views/frontend/waykambas.blade.php
@@ -8,7 +8,7 @@
     @include('partials.topbar')
     @include('partials.heroPage')
      <!-- content -->
-     <div class="flex flex-col space-y-6 max-w-3xl mx-auto py-12 px-4">
+     <div class="flex flex-col space-y-6 max-w-2xl mx-auto py-12 px-4">
         <div class="prose max-w-none ma">
             {!! $content->textwaykambas !!}
         </div>
