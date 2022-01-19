7dcc98951e1dbe92ed4472bb3b9419b358f4cded
ultramenid <malichamdan@gmail.com>
2022-01-19 22:05:15 +0700

    add apect ratio

--- resources/views/frontend/waykambas.blade.php
+++ resources/views/frontend/waykambas.blade.php
@@ -8,8 +8,8 @@
     @include('partials.topbar')
     @include('partials.heroPage')
      <!-- content -->
-     <div class="flex flex-col space-y-6 max-w-2xl mx-auto py-12 px-4">
-        <div class="prose max-w-none ma">
+     <div class="flex flex-col space-y-6 max-w-5xl mx-auto py-12 px-4">
+        <div class="prose max-w-none ">
             {!! $content->textwaykambas !!}
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -33,5 +33,6 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
   ],
 }
