87908ff46b3f8012fe5f686c68eacea595fd350f
ultramenid <malichamdan@gmail.com>
2022-01-20 21:47:15 +0700

    dummy css

--- resources/views/partials/dummycss.blade.php
+++ resources/views/partials/dummycss.blade.php
@@ -4,3 +4,14 @@
     </div>
 
 </div>
+
+
+<div class="mx-4 bg-white shadow-xl p-8 text-slate-700 dark:bg-slate-800 dark:text-slate-400">
+    <div class="sm:float-left float-none mr-6">
+      <div class="relative w-full sm:w-44">
+        <img class="object-cover rounded-lg w-full" src="https://i.ibb.co/gDX1MWG/Whats-App-Image-2022-01-20-at-4-36-18-PM.jpg" />
+        <div class="absolute inset-0 ring-1 ring-inset ring-black/10 rounded-lg"></div>
+      </div>
+    </div>
+    <p class="text-justify">Maybe we can live without libraries, people like you and me. Maybe. Sure, we're too old to change the world, but what about that kid, sitting down, opening a book, right now, in a branch at the local library and finding drawings of pee-pees and wee-wees on the Cat in the Hat and the Five Chinese Brothers? Doesn't HE deserve better? Look. If you think this is about overdue fines and missing books, you'd better think again. This is about that kid's right to read a book without getting his mind warped! Or: maybe that turns you on, Seinfeld; maybe that's how y'get your kicks. You and your good-time buddies.</p>
+  </div>
