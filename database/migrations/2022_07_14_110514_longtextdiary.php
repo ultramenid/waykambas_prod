c48cb375b76c728d02a428ab97c91252475da9d2
ultramenid <malichamdan@gmail.com>
2022-07-14 18:06:15 +0700

    longtext diary

---
+++ database/migrations/2022_07_14_110514_longtextdiary.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('greendiary', function (Blueprint $table) {
+            $table->longText('imgDescEN')->change();
+            $table->longText('imgDescID')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('greendiary', function (Blueprint $table) {
+            $table->string('kronologi')->change();
+            $table->string('perkembangankasus')->change();
+        });
+    }
+};
