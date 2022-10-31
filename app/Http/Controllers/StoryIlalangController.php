6721d100e82a2c86b9c5f0f2caf97cc98daa2a31
ultramenid <malichamdan@gmail.com>
2022-10-31 19:52:53 +0700

    new ogilalang

--- app/Http/Controllers/StoryIlalangController.php
+++ app/Http/Controllers/StoryIlalangController.php
@@ -17,7 +17,7 @@ class StoryIlalangController extends Controller
         $text = $this->getStory()->titleID;
         $imageHero = 'ilalang.jpg';
         $nav = 'story';
-        $ogimage = 'ogilalang300.jpeg';
+        $ogimage = 'ogilalang3150.jpeg';
         $publish = date("F Y", strtotime($this->getStory()->publishdate)) ;
         $desc = 'Belantara ilalang membalut area seluas 40.780 ha di Way Kambas. Medan berat inilah yang kami taklukkan untuk menemukan titik awal restorasi di Rawa Kadut.';
         $imgdesc = 'Gambar 1. Belantara ilalang di Way Kambas. Tak ada jalan setapak, semua tertutup ilalang. Medan berat seperti inilah yang kami taklukkan untuk menemukan titik awal restorasi di Rawa Kadut. @ Koen Setyawan / SILVAGAMA';
