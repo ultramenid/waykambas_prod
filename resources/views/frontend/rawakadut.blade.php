8378e682729cdc19566443e54a09c22d971ffcf4
ultramenid <malichamdan@gmail.com>
2022-11-15 11:54:40 +0700

    restore rawakadut page

--- resources/views/frontend/rawakadut.blade.php
+++ resources/views/frontend/rawakadut.blade.php
@@ -6,8 +6,13 @@
 
 @section('content')
     @include('partials.topbar')
-
-
+    @include('partials.heroPage')
+     <!-- content -->
+     <div class="flex flex-col space-y-6 max-w-2xl mx-auto py-2 px-4">
+        <div class="prose max-w-none">
+            {!! $content->textrawakadut !!}
+        </div>
+    </div>
 
 
     @include('partials.footer')
