62919d93ada7041417fcd7595f3b06e7e5d7f308
ultramenid <malichamdan@gmail.com>
2022-11-17 14:23:11 +0700

    vide cant be play at same time

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -44,5 +44,20 @@
             behaviour: 'smooth'
         })
     })
+    // assume only one video is playing at a time
+    var videoPlaying = null;
+
+    const onPlay = function() {
+    if (videoPlaying && videoPlaying != this) {
+        videoPlaying.pause()
+    }
+    videoPlaying = this
+    }
+
+    // init event handler
+    const videos = document.getElementsByClassName("video-bg")
+    for (let i = 0; i < videos.length; i++) {
+    videos[i].addEventListener("play", onPlay)
+    }
 </script>
 </html>
