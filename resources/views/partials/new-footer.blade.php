63e757e66aa2ac32baf3160af2703edb997b06d5
ultramenid <malichamdan@gmail.com>
2024-01-06 13:02:59 +0700

    footer

--- resources/views/partials/new-footer.blade.php
+++ resources/views/partials/new-footer.blade.php
@@ -29,11 +29,11 @@
     <div class="max-w-6xl mx-auto px-4 border-t border-gray-200 mt-12"></div>
     <div class="max-w-6xl mx-auto px-4 border-t border-gray-400 mt-24"></div>
 
-    <div class="max-w-5xl mx-auto px-4">
-        <div class="sm:gap-16 gap-6 flex sm:flex-row flex-col items-center mt-6">
+    <div class="max-w-3xl mx-auto px-4">
+        <div class="sm:gap-16 gap-6 flex sm:flex-row flex-col mt-6 ">
             <img src="{{ asset('img/rawakadut-logo.png') }}" alt="" class="h-16">
-            <div class="">
-                <p>Memulihkan ekosistem Way Kambas sehingga menjadi habitat yang aman dan nyaman bagi satwa setempat, seperti gajah, harimau, badak.</p>
+            <div class="w-6/12">
+                <p class="">Memulihkan ekosistem Way Kambas sehingga menjadi habitat yang aman dan nyaman bagi satwa setempat, seperti gajah, harimau, badak.</p>
                 <div class="flex sm:flex-row flex-col mt-4 text-xs">
                     <a >© 2024 Auriga Nusantara. All rights reserved.</a>
 
