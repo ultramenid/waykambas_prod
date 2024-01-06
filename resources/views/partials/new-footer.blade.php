7a6b3d93b5d9220930f89bebeb520e70ff4b96e8
ultramenid <malichamdan@gmail.com>
2024-01-06 12:54:11 +0700

    footer

--- resources/views/partials/new-footer.blade.php
+++ resources/views/partials/new-footer.blade.php
@@ -2,26 +2,26 @@
     <div class="max-w-5xl mx-auto px-4">
         <div class="flex sm:flex-row flex-col justify-between gap-12">
             <div class="flex flex-col gap-3">
-                <a class="uppercase text-3xl font-black">About</a>
+                <a class="uppercase text-2xl font-black">About</a>
                 <a class="text-xl font-satunya font-bold">who we are</a>
                 <a class="text-xl font-satunya font-bold">way kambas</a>
                 <a class="text-xl font-satunya font-bold">restorasi rawa kadut</a>
             </div>
             <div class="flex flex-col gap-3">
-                <a class="uppercase text-3xl font-black">A to Z Restorasi</a>
+                <a class="uppercase text-2xl font-black">A to Z Restorasi Kadut</a>
                 <a class="text-xl font-satunya font-bold">konteks</a>
                 <a class="text-xl font-satunya font-bold">silvikultur</a>
-                <a class="text-xl font-satunya font-bold">Penanggulangan kebakaran</a>
+                <a class="text-xl font-satunya font-bold">penanggulangan kebakaran</a>
                 <a class="text-xl font-satunya font-bold">pelibatan masyarakat</a>
             </div>
             <div class="flex flex-col gap-3">
-                <a class="uppercase text-3xl font-black">Feature</a>
+                <a class="uppercase text-2xl font-black">Feature</a>
             </div>
             <div class="flex flex-col gap-3">
-                <a class="uppercase text-3xl font-black">Diary</a>
+                <a class="uppercase text-2xl font-black">Diary</a>
             </div>
             <div class="flex flex-col gap-3">
-                <a class="uppercase text-3xl font-black">Kinerja</a>
+                <a class="uppercase text-2xl font-black">Kinerja</a>
             </div>
         </div>
     </div>
