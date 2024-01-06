5a894724e714c4f306425ab6e542a7e1918386ca
ultramenid <malichamdan@gmail.com>
2024-01-06 15:56:06 +0700

    footer

--- resources/views/partials/new-footer.blade.php
+++ resources/views/partials/new-footer.blade.php
@@ -31,7 +31,7 @@
 
     <div class="max-w-3xl mx-auto px-4">
         <div class="sm:gap-16 gap-6 flex sm:flex-row flex-col mt-6 ">
-            <img src="{{ asset('img/rawakadut-logo.png') }}" alt="" class="sm:h-16 h-20 w-full">
+            <img src="{{ asset('img/rawakadut-logo.png') }}" alt="" class="sm:h-16 h-20 ">
             <div class="sm:w-6/12 w-full">
                 <p class="">Memulihkan ekosistem Way Kambas sehingga menjadi habitat yang aman dan nyaman bagi satwa setempat, seperti gajah, harimau, badak.</p>
                 <div class="flex sm:flex-row flex-col mt-4 text-xs">
