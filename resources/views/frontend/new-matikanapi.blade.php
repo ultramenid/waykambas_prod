cd5bc1dd40752856040c4c82f232495025d8ae74
ultramenid <malichamdan@gmail.com>
2024-01-11 22:16:26 +0700

    link

--- resources/views/frontend/new-matikanapi.blade.php
+++ resources/views/frontend/new-matikanapi.blade.php
@@ -14,7 +14,7 @@
             <div class="flex flex-col gap-8 sm:w-8/12 w-full">
                 <h1 class="font-satunya text-white font-bold text-2xl uppercase">Feature</h1>
                 <h1 class="text-5xl font-black text-white ">Matikan Api Menjaga Puspa</h1>
-                <p class="text-white">Fajar Sandhikan dan timnya melawan api yang melahap ladang ilalang di Rawa Kadut, Taman Nasional Way Kambas, Lampung. Hasil kerja mereka berhasil menyelamatkan program reforestasi 100 hektare dengan sekitar 20 ribu pohon.</p>
+                <p class="text-white">Fajar Sandhika dan timnya melawan api yang melahap ladang ilalang di Rawa Kadut, Taman Nasional Way Kambas, Lampung. Hasil kerja mereka berhasil menyelamatkan program reforestasi 100 hektare dengan sekitar 20 ribu pohon.</p>
                 <img src="{{ asset('assets/hazeid.png') }}" alt="Matikan Api Menjaga Puspa" class="sm:w-52 w-full mb-12">
             </div>
         </div>
